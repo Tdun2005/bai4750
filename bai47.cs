3406c821709c3f96e2be752ba0c8de1dafddaaa5
Tdun2005 <lqd9a1tungduongphamnguyen@gmail.com>
2024-07-04 13:47:03 +0700

    Create bai47.cs

---
+++ bai47.cs
@@ -0,0 +1,7 @@
+Trong đoạn mã sử dụng kiểu Dictionary, key là trường đầu tiên trong mỗi cặp key-value.
+  Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+myDictionary.Add("Apple", 1);
+myDictionary.Add("Banana", 2);
+myDictionary.Add("Cherry", 3);
+=> các key là "Apple", "Banana" và "Cherry".
